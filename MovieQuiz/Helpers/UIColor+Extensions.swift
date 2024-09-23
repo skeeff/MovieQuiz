67c9c3dfc1d53b25148912b49e9039f373ab35ed
fedar <fedorch2012@gmail.com>
2024-09-23 22:50:24 +0300

    выполнение задачи 4го спринта

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+    var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+    var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+}
